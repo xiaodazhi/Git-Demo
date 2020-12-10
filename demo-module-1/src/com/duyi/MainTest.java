ece124827e2b8bb0edfc4c4f682fefc82179c7ee
xiaodazhi <xiaodazhi_414@163.com>
2020-12-10 17:57:37 +0800

    master分支修改  hello master

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -9,6 +9,6 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
-        System.out.println("hello test");
+        System.out.println("hello master");
     }
 }
